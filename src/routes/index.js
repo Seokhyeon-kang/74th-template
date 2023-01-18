d94b1c49092fd0e75418e8eaeec090e837fc3aa0
Seokhyeon-kang <rkdtjrgus21c@naver.com>
2023-01-18 15:05:37 +0900

    ~

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { useRoutes } from 'react-router-dom';
 
 // routes
-import AuthenticationRotes from './AuthenticationRoutes';
+import AuthenticationRoutes from './AuthenticationRoutes';
 import LoginRoutes from './LoginRoutes';
 import MainRoutes from './MainRoutes';
 import RootRoute from "../root/RootRoute";
@@ -11,5 +11,11 @@ import LogiRootRoute from "../erp/logistic/root/RootRoute";
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-    return useRoutes([LoginRoutes, AuthenticationRotes, MainRoutes, ...RootRoute]);
+    return useRoutes([LoginRoutes, AuthenticationRoutes, MainRoutes, ...RootRoute]);
 }
+
+// 오타 수정 : AuthenticationRotes -> AuthenticationRoutes(import부분도 수정 완료!)
+
+
+// ㅇㄹㄶㄴㅇㅎㄴㅇ런ㅇ'란ㅇ;ㅣㄹㅇㄹㄴ마ㅣㄹ;ㅓ니아ㅓ리나얼ㄴ이;런;ㅣㄹ
+
